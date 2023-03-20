4110a2c7d71468c09c4dd6260be220c97d43202e
marcoshenke <marcos.henke3@gmail.com>
2023-03-20 20:20:16 -0300

    progredindo na tentativa de fazer uma requisição na api com useEffect

--- desafiofinalreact/src/views/Teams/Teams.js
+++ desafiofinalreact/src/views/Teams/Teams.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import {NavBar, FooterPag} from 'components'
-import {Box} from '@mui/material'
+import {Box, FormLabel, Button} from '@mui/material'
+import api from '../../service/api'
 
 const Teams = () => {
-    const [inputTeam, setInputTeam] = useState('');
-    const [apiData, setApiData] = useState([]);
+    const [teams, setTeams] = useState([])
+     
+    useEffect(() => {
+        const getTeams = async () => {
+            const teams = await api.get('/teams', {params: {per_page: 30}})
+            console.log(teams)
+            setTeams(teams?.data)
+        }
 
-    const fetchData = async () => {
-        const response = await fetch(`teams${inputValue}`);
-        const data = await response.json();
-        setApiData(data);
-      };
+        getTeams()
+    }, [])
       
 
 
