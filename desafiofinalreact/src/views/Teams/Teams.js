74a9b69bf210109b8807624dbf51f4c3a931c863
marcoshenke <marcos.henke3@gmail.com>
2023-03-21 07:21:35 -0300

    api pag teams

--- desafiofinalreact/src/views/Teams/Teams.js
+++ desafiofinalreact/src/views/Teams/Teams.js
@@ -1,39 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import {NavBar, FooterPag} from 'components'
-import {Box, FormLabel, Button} from '@mui/material'
-import api from '../../service/api'
+import React, { useEffect, useState } from "react";
+import { NavBar, FooterPag } from "components";
+import {
+  Box,
+  FormLabel,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import api from "../../service/api";
 
 const Teams = () => {
-    const [teams, setTeams] = useState([])
-     
-    useEffect(() => {
-        const getTeams = async () => {
-            const teams = await api.get('/teams', {params: {per_page: 30}})
-            console.log(teams)
-            setTeams(teams?.data)
-        }
+  const [teams, setTeams] = useState([]);
 
-        getTeams()
-    }, [])
-      
+  useEffect(() => {
+    const getTeams = async () => {
+      const teams = await api.get("/teams", { params: { per_page: 30 } });
+      console.log(teams.data.data);
+      setTeams(teams.data.data);
+    };
 
+    getTeams();
+  }, []);
 
-    
-    
-    return (
-        <Box
-        sx={{
-            width: "100%",
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-          }}
-        >
-            <NavBar />
-            <FooterPag />
-        </Box>
-    )
-}
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <NavBar />
+      <Box>
+        Esse são todos os times da NBA:
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="right">Nome</TableCell>
+                <TableCell align="right">Cidade</TableCell>
+                <TableCell align="right">Conferência</TableCell>
+                <TableCell align="right">Abreviação</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {teams?.map((team) => (
+                <TableRow>
+                  <TableCell>{teams?.full_name}</TableCell>
+                  <TableCell>{teams?.city}</TableCell>
+                  <TableCell>{teams?.conference}</TableCell>
+                  <TableCell>{teams?.abbreviation}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+      <FooterPag />
+    </Box>
+  );
+};
 
-export default Teams
+export default Teams;
